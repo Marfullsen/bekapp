2d9911fbb902741d74b8d341eeb1c5d67e608e04
Francisco Marfull <marfullsen@gmail.com>
2022-09-25 06:33:30 -0300

    ADD: Primer componente de prueba.

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,12 +1,11 @@
+<script setup>
+import DropDown from '../components/DropDown.vue';
+</script>
+
 <template>
   <div class="container py-4 px-3 mx-auto">
-      <h1>Yeppppp</h1>
-      <button class="btn btn-primary">Súper Botón</button>
-    </div>
+    <h1>Yeppppp</h1>
+    <button class="btn btn-primary">Súper Botón</button>
+  </div>
+  <drop-down></drop-down>
 </template>
-
-<script>
-export default {
-  name: "MainView",
-};
-</script>
