0c0fb70f963c5c1a2c3d3a9685786f3127e74d79
Francisco Marfull <marfullsen@gmail.com>
2022-10-02 10:13:05 -0300

    ADD:[Cliente] Add views cliente (display).

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,20 @@
 import { createRouter, createWebHistory } from "vue-router";
 import About from "../views/About.vue";
 import NotFound from "../views/NotFound.vue";
+
 import DashboardGrid from "../views/DashboardGrid.vue";
+import ShowClientes from "../views/cliente/ShowClientes.vue";
+import CreateCliente from "../views/cliente/CreateCliente.vue";
+
 
 const routes = [
   { path: "/", component: DashboardGrid },
   { path: "/about", component: About },
+
   { path: "/dashboard",  name: "dashboard", component: () => import("../views/DashboardGrid.vue")},
+  { path: "/cliente", name: "cliente", component: ShowClientes },
+  { path: "/cliente/nuevo", name: "create-cliente", component: CreateCliente },
+
   {
     path: '/:pathMatch(.*)*',
     component: NotFound,
---
+++ src/views/cliente/ShowClientes.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <h1 class="d-flex justify-content-center">Clientes</h1>
+    <p class="d-flex justify-content-center text-muted">
+      Sin clientes registrados.
+    </p>
+    <div id="floating-button" @click="handleCreateCliente">
+      <p class="plus noselect">+</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    handleCreateCliente() {
+      this.$router.push('/cliente/nuevo/')
+    }
+  }
+};
+</script>
+
+<style scoped>
+#floating-button {
+  width: 55px;
+  height: 55px;
+  border-radius: 50%;
+  background: #db4437;
+  position: fixed;
+  bottom: 30px;
+  right: 30px;
+  cursor: pointer;
+  box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.2);
+}
+
+#floating-button:hover {
+  box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.5);
+}
+
+.plus {
+  color: white;
+  position: absolute;
+  top: 0;
+  display: block;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  text-align: center;
+  padding: 0;
+  margin: 0;
+  line-height: 55px;
+  font-size: 38px;
+  font-family: "Roboto";
+  font-weight: 300;
+}
+</style>
