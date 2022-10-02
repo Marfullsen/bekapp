fefc2edd897af41e9d62600e378dba14a54fd84a
Francisco Marfull <marfullsen@gmail.com>
2022-10-02 02:07:34 -0300

    ADD:[LoginView] Add non-sense view. (Refactor soon)

---
+++ src/views/LoginView.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="container">
+    <LoginForm/>
+  </section>
+</template>
+
+<script>
+import LoginForm from "../components/LoginForm.vue";
+
+export default {
+  components: {
+    LoginForm
+  },
+};
+</script>
+
+<style scoped></style>
