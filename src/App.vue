2fee83efbfca3213360cb22f03174d799c8e08bd
Francisco Marfull <marfullsen@gmail.com>
2022-10-02 01:46:27 -0300

    ADD:[Layouts] Implementación de layouts artesanales.

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,25 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { useUserStore } from "./stores/user";
+const user = useUserStore();
 </script>
 
 <template>
-  <nav>
-    <router-link to="/">
-      <img src="./assets/logo_beka.webp" alt="Logo" style="width:156px;height:60px;object-fit:cover">
-    </router-link>&nbsp;
-    <router-link to="/about">
-      <svg xmlns="http://www.w3.org/2000/svg" width="44" height="44" fill="black" class="bi bi-list" viewBox="0 0 16 16">
-        <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
-      </svg>
-    </router-link>
-  </nav>
-  <router-view class="router-view"/>
+  <logged-user v-if="user.isLogged"/>
+  <Default v-else>
+    <login-view/>
+  </Default>
 </template>
 
-<style scoped>
-  nav {
-    display: flex;
-    justify-content: space-around;
-    padding-top: 0.51rem;
-  }
+<script>
+import Default from "./layouts/Default.vue";
+import LoginView from "./views/LoginView.vue";
+import LoggedUser from "./layouts/LoggedUser.vue";
 
-  .router-view {
-    padding-top: 15px;
-    padding-bottom: 15px;
-  }
-</style>
+export default {
+  components: {
+    Default, LoginView, LoggedUser
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ src/layouts/LoggedUser.vue
@@ -0,0 +1,50 @@
+<template>
+  <nav>
+    <router-link to="/">
+      <img src="../assets/logo_beka.webp" alt="Logo" style="width:156px;height:60px;object-fit:cover">
+    </router-link>&nbsp;
+    <div class="dropdown">
+      <svg id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false" xmlns="http://www.w3.org/2000/svg" width="44" height="44" fill="black" class="bi bi-list" viewBox="0 0 16 16">
+        <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+      </svg>
+      <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+        <li><a class="dropdown-item" href="#">Cambiar clave</a></li>
+        <li><a class="dropdown-item" href="#">Mi empresa</a></li>
+        <li><a class="dropdown-item" href="#">Administrar usuario</a></li>
+        <li><a class="dropdown-item" href="#">Administrar contacto</a></li>
+        <li><a class="dropdown-item" href="#">Administrar empresa</a></li>
+        <li><a class="dropdown-item" @click="logout">Salir</a></li>
+      </ul>
+    </div>
+    <router-link to="/about">
+    </router-link>
+  </nav>
+  <router-view class="router-view"/>
+</template>
+
+<script>
+import { useUserStore } from "../stores/user";
+
+  export default {
+    methods: {
+      logout() {
+        const user = useUserStore();
+        user.logout()
+        this.$router.push('/login')
+      }
+    }
+  };
+  </script>
+
+<style scoped>
+  nav {
+    display: flex;
+    justify-content: space-around;
+    padding-top: 0.51rem;
+  }
+
+  .router-view {
+    padding-top: 15px;
+    padding-bottom: 15px;
+  }
+</style>
