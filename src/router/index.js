e595812a0cbb67d59ace07c573b9129c8346a93f
Francisco Marfull <marfullsen@gmail.com>
2022-10-02 02:02:45 -0300

    DEL:[Router] Delete unsused views from Router.

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,10 @@
 import { createRouter, createWebHistory } from "vue-router";
-import MainView from "../views/MainView.vue";
 import About from "../views/About.vue";
 import NotFound from "../views/NotFound.vue";
 import DashboardGrid from "../views/DashboardGrid.vue";
 
 const routes = [
-  { path: "/", component: MainView },
+  { path: "/", component: DashboardGrid },
   { path: "/about", component: About },
   { path: "/dashboard",  name: "dashboard", component: () => import("../views/DashboardGrid.vue")},
   {
