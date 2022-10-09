30aec26f3416d3f9f6ecd3172d1ca3dc9f6e3be1
Francisco Marfull <marfullsen@gmail.com>
2022-10-09 09:47:07 -0300

    ADD:[Production] Agregar deploy para producción.

---
+++ deploy.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env sh
+
+# Current date
+timestamp=$(date +%Y-%m-%d' '%H:%M)
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# place .nojekyll to bypass Jekyll processing
+echo > .nojekyll
+
+# Create 404 same as index for proper vue-routing redirecting.
+cp index.html 404.html
+
+git init
+git add -A
+git commit -m "deploy at $timestamp"
+
+git push -f https://github.com/Marfullsen/bekapp.git master:gh-pages
+
+cd -
