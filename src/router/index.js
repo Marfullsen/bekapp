83a32c82542ee72245014b085aa916b10433770f
Francisco Marfull <marfullsen@gmail.com>
2022-10-10 00:50:05 -0300

    ADD:[Router] Agregar rutas del Stock.

--- src/router/index.js
+++ src/router/index.js
@@ -3,17 +3,38 @@ import About from "../views/About.vue";
 import NotFound from "../views/NotFound.vue";
 
 import DashboardGrid from "../views/DashboardGrid.vue";
+
 import ShowClientes from "../views/cliente/ShowClientes.vue";
 import CreateCliente from "../views/cliente/CreateCliente.vue";
 
+import MenuStock from "../views/stock/MenuStock.vue";
+import StockRiego from "../views/stock/StockRiego.vue";
+import StockConsumibles from "../views/stock/StockConsumibles.vue";
+import StockHerramientas from "../views/stock/StockHerramientas.vue";
+import StockUniformes from "../views/stock/StockUniformes.vue";
+import RiegoMaterial from "../views/stock/RiegoMaterial.vue";
+
 
 const routes = [
+  // Rutas de la página.
+  // { path: "/", name: "", component:  },
+
   { path: "/", component: DashboardGrid },
   { path: "/about", component: About },
 
   { path: "/dashboard",  name: "dashboard", component: () => import("../views/DashboardGrid.vue")},
+
+  // Cliente.
   { path: "/cliente", name: "cliente", component: ShowClientes },
   { path: "/cliente/nuevo", name: "create-cliente", component: CreateCliente },
+
+  // Stock.
+  { path: "/stock", name: "stock", component: MenuStock },
+  { path: "/stock/riego", name: "stock_riego", component: StockRiego },
+  { path: "/stock/riego/:lugarCamioneta/", name: "riego_material", component: RiegoMaterial },
+  { path: "/stock/consumibles", name: "stock_consumibles", component: StockConsumibles },
+  { path: "/stock/herramientas", name: "stock_herramientas", component: StockHerramientas },
+  { path: "/stock/uniformes", name: "stock_uniformes", component: StockUniformes },
 
   {
     path: '/:pathMatch(.*)*',
